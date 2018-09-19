04abb1c943b737e57cad64cb057995093f6e0ea0
BlackLunarFang <dhruvwaghela1994@gmail.com>
2018-09-19 19:24:37 +0530

    This is just basic
    Here I've just created a Cube rolling

---
+++ 3Dto2D/Assets/Scripts/FollowCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCube : MonoBehaviour {
+
+    public GameObject Cube;
+    public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - Cube.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Cube.transform.position + offset;
+    }
+}
---
+++ 3Dto2D/Assets/Scripts/RollingCube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RollingCube : MonoBehaviour {
+    public float speed = 5;
+	// Use this for initialization
+	void Start () {
+		    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            GetComponent<Rigidbody>().angularVelocity = Vector3.forward * Input.GetAxisRaw("Vertical") * speed;
+        }
+            if (Input.GetAxisRaw("Horizontal")!=0){
+            GetComponent<Rigidbody>().angularVelocity = Vector3.right * Input.GetAxisRaw("Horizontal") * speed;
+        }
+
+	}
+}
